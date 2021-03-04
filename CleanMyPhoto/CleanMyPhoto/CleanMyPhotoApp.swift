77b949138a6de747526f202343a365239a6567ed
Volodymyr Mudrik <volodymyrmudrik@Volodymyrs-MacBook-Pro.local>
2021-03-04 20:22:13 +0200

    project initiated in xcode

---
+++ CleanMyPhoto/CleanMyPhoto/CleanMyPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanMyPhotoApp.swift
+//  CleanMyPhoto
+//
+//  Created by Volodymyr Mudrik on 02.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct CleanMyPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanMyPhoto/CleanMyPhoto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CleanMyPhoto
+//
+//  Created by Volodymyr Mudrik on 02.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hi, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
