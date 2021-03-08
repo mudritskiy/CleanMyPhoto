67b0359761a4ac4f4737efc39538de59f2b0555c
Volodymyr Mudrik <volodymyrmudrik@Volodymyrs-MacBook-Pro.local>
2021-03-08 20:37:56 +0200

    photo test

--- CleanMyPhoto/CleanMyPhoto/ContentView.swift
+++ CleanMyPhoto/CleanMyPhoto/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isShowPhotoLibrary = false
+    @State private var image = UIImage()
+    
     var body: some View {
-        Text("Hi, world!")
-            .padding()
+        
+        VStack {
+            
+            Image(uiImage: self.image)
+                .resizable()
+                .scaledToFit()
+                //.frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            Button(action: {
+                self.isShowPhotoLibrary = true
+            }) {
+                HStack {
+                    Image(systemName: "photo")
+                        .font(.system(size: 20))
+                    Text("Photo library")
+                        .font(.headline)
+                }
+                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxHeight: 50)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(20)
+                .padding(.horizontal)
+            }
+        }
+        .sheet(isPresented: $isShowPhotoLibrary) {
+            ImagePicker(sourceType: .photoLibrary, selectedImage: self.$image)
+        }
+        
     }
 }
 
---
+++ CleanMyPhoto/CleanMyPhoto/ImagePicker.swift
@@ -0,0 +1,52 @@
+//
+//  ImagePicker.swift
+//  CleanMyPhoto
+//
+//  Created by Volodymyr Mudrik on 04.03.2021.
+//
+
+import UIKit
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+  
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    
+    @Binding var selectedImage: UIImage
+    @Environment(\.presentationMode) private var presentationMode
+    
+    final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = context.coordinator
+        
+        return imagePicker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: UIViewControllerRepresentableContext<ImagePicker>) {
+        
+    }
+    
+}
+
