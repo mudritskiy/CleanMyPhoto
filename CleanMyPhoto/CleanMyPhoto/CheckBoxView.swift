1d8b0c4e88163ac007c9055705b5cd5cef070c2b
Volodymyr Mudrik <volodymyrmudrik@R6C39A.pup.local>
2021-03-14 21:51:15 +0200

    implemented intaction beetween checked states sections and cells

--- CleanMyPhoto/CleanMyPhoto/CheckBoxView.swift
+++ CleanMyPhoto/CleanMyPhoto/CheckBoxView.swift
@@ -9,10 +9,14 @@ import SwiftUI
 
 struct CheckBoxView: View {
 
-    @Binding var checked: Bool
+    @Binding var checked: Int
     @Binding var trimValue: CGFloat
 
-    let opacityUnchecked: Double = 0.8
+    @Binding var sectionChecked: Int
+    @Binding var sectionTrimValue: CGFloat
+
+    @State var size: CGFloat = 30
+    @State var showOverlay: Bool = true
 
     var animatbleData: CGFloat {
         get { trimValue }
@@ -21,23 +25,27 @@ struct CheckBoxView: View {
 
     var body: some View {
 
-        let size: CGFloat = 30
-        let sizeInner = size * 0.75
+        let sizeInner = size * (showOverlay ? 0.75 : 1)
+        let opacityUnchecked: Double = 0.9
+        let color: Color = (showOverlay ? Color.white : Color.color1).opacity(opacityUnchecked)
+
+        let checkState = checked ==  1 || sectionChecked == 1
+        let trimState = max(trimValue, sectionTrimValue)
 
         ZStack {
             Circle()
-                .trim(from: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, to: trimValue)
+                .trim(from: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, to: trimState)
                 .stroke(style: StrokeStyle(lineWidth: 2))
                 .frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                .foregroundColor(checked ? .white : Color.white.opacity(opacityUnchecked))
-                .shadow(radius: 5)
+                .foregroundColor(checkState ? .white : color)
                 .overlay(
                     Circle()
-                        .fill(checked ? .green : Color.white.opacity(opacityUnchecked))
-                        .frame(width: sizeInner, height: sizeInner, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/))
-            if checked {
+                        .fill(checkState ? .blue : color)
+                .frame(width: sizeInner, height: sizeInner, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/))
+            if checkState {
                 Image(systemName: "checkmark")
                     .foregroundColor(.white)
+                    .font(.system(size: 13))
             }
         }
     }
